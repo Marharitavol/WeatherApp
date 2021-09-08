d63e3b1f7c347626ed27150a7b523d096a731dbb
Marharitavol <marharitavol@gmail.com>
2021-09-08 17:24:34 +0300

    Create storyboard content and outlets

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let icon = WeatherIconManager.Rain.image
+        let currentWeather = CurrentWeather(temperature: 10.0, appearentTemperature: 5.0, humidity: 30, pressure: 750, icon: icon)
+        updateUIWith(currentWeather: currentWeather)
+        
+       
     }
 
-
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    func updateUIWith(currentWeather: CurrentWeather) {
+        
+        self.imageView.image = currentWeather.icon
+        self.pressureLabel.text = currentWeather.pressureString
+        self.temperatureLabel.text = currentWeather.temperatureString
+        self.appearentTemperatureLabel.text = currentWeather.appearentTemperatureString
+        self.humidityLabel.text = currentWeather.humidityString
+    }
 }
 
